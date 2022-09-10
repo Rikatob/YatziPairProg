4502732dbd561b44965fc242c31fd68407df7c22
Rikatob <89411878+Rikatob@users.noreply.github.com>
2022-09-10 14:29:16 +0200

    🔵 * Converted the if statements to Switch Case. * Extracted getDiceCount to method. * Extracted getPairScore to method.

--- src/main/java/no/kristiania/yatzi/Yatzi.java
+++ src/main/java/no/kristiania/yatzi/Yatzi.java
@@ -16,6 +16,21 @@ public class Yatzi {
     public int scoreAs(YatziCombination type) {
         int score = 0;
 
+        Map<Integer, Integer> diceCount = getDiceCount();
+
+        switch (type) {
+            case CHANCE -> score = 15;
+            case ONES -> score = diceCount.get(1);
+            case TWOS -> score = (diceCount.get(2) * 2);
+            case THREES -> score = (diceCount.get(3) * 3);
+            case PAIR -> score = getPairScore(score, diceCount);
+        }
+
+        return score;
+
+    }
+
+    private Map<Integer, Integer> getDiceCount() {
         Map<Integer, Integer> diceCount = new HashMap<>();
         for (int dice : diceRoll) {
             if (!diceCount.containsKey(dice)) {
@@ -24,35 +39,19 @@ public class Yatzi {
                 diceCount.put(dice, diceCount.get(dice) + 1);
             }
         }
+        return diceCount;
+    }
 
-        if (type.equals(YatziCombination.CHANCE)) {
-            score = 15;
-        }
-        if (type.equals(YatziCombination.ONES)) {
-            score = diceCount.get(1);
-        }
-        if (type.equals(YatziCombination.TWOS)) {
-            score = (diceCount.get(2) * 2);
-        }
-        if (type.equals(YatziCombination.THREES)) {
-            score = (diceCount.get(3) * 3);
-        }
-        if (type.equals(YatziCombination.PAIR)) {
-
-            for (int key : diceCount.keySet()) {
-                if (diceCount.get(key) >= 2) {
-                    if ((key * 2) > score) {
-                        score = key * 2;
-
-                    }
+    private int getPairScore(int score, Map<Integer, Integer> diceCount) {
+        for (int key : diceCount.keySet()) {
+            if (diceCount.get(key) >= 2) {
+                if ((key * 2) > score) {
+                    score = key * 2;
 
                 }
+
             }
-
         }
-
-
         return score;
-
     }
 }
