e26909612c682d22679785a8bd3edc47ba6f34b4
Fargekritt <lullinj98@gmail.com>
2022-09-10 13:28:01 +0200

    🔵 * added field * added constructor * threw on hashmap to count dice hendelser

--- src/main/java/no/kristiania/yatzi/Yatzi.java
+++ src/main/java/no/kristiania/yatzi/Yatzi.java
@@ -1,21 +1,38 @@
 package no.kristiania.yatzi;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Yatzi {
+
+    private int[] diceRoll;
+
     public Yatzi(int[] diceRoll) {
+        this.diceRoll = diceRoll;
     }
 
 
     public int scoreAs(YatziCombination type) {
         int score = 0;
 
+        Map<Integer, Integer> diceCount = new HashMap<>();
+        for (int dice : diceRoll) {
+            if(!diceCount.containsKey(dice)){
+                diceCount.put(dice,1);
+            } else {
+                diceCount.put(dice,diceCount.get(dice)+1);
+            }
+        }
+
+
         if (type.equals(YatziCombination.CHANCE)) {
             score = 15;
         }
         if (type.equals(YatziCombination.ONES)) {
-            score = 2;
+            score = diceCount.get(1);
         }
         if (type.equals(YatziCombination.TWOS)) {
-            score = 4;
+            score = (diceCount.get(2)*2);
         }
         return score;
 
