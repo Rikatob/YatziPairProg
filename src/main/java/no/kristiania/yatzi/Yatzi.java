b8d71347d1efcd00dfd67aa82f4e8d8bbdfb7b13
Rikatob <rikatob@hotmail.com>
2022-09-10 20:06:58 +0200

    :bulb: For Yatzi.

--- src/main/java/no/kristiania/yatzi/Yatzi.java
+++ src/main/java/no/kristiania/yatzi/Yatzi.java
@@ -14,6 +14,9 @@ public class Yatzi {
     }
 
 
+    /**--------------------------------------------------------------------
+     * -> Calculate the score of the different combinations in Yatzi game.
+     ----------------------------------------------------------------------*/
     public int scoreAs(YatziCombination type) {
         int score = 0;
 
@@ -37,6 +40,11 @@ public class Yatzi {
 
     }
 
+
+    /**--------------------------------------------------------------------------
+     * -> Uses one diceRoll to put the dice in a hashmap with key/value as integers.
+     * -> key = The dice type <---> value = The number of times the dice occur.
+     ---------------------------------------------------------------------------*/
     private Map<Integer, Integer> getDiceCount() {
         Map<Integer, Integer> diceCount = new HashMap<>();
         for (int dice : diceRoll) {
@@ -49,6 +57,14 @@ public class Yatzi {
         return diceCount;
     }
 
+
+    /**--------------------------------------------------------------------------------------------
+     * -> Goes through every key (diceType)
+     * -> Checks if the value (times the dice occur) is equal or more than 2 (one or more pair)
+     *      -> Add the right score for the first pair found in the map.
+     *          -> Then checks if pair occur more than once
+     *             and if it does it change score value to the highest possible.
+     -----------------------------------------------------------------------------------------------*/
     private int getPairScore(int score, Map<Integer, Integer> diceCount) {
 
         for (int key : diceCount.keySet()) {
@@ -62,6 +78,14 @@ public class Yatzi {
     }
 
 
+    /**----------------------------------------------------------------------------------
+     * -> Does the same as getPairScore(), but just add the key (type of dice) to score.
+     * -> Multiply the score with two to get the right score.
+     * -> (key <--> value)
+     *      -> (1 <--> 2), (4 <--> 2), (6 <--> 1)
+     *          -> two pairs
+     *              -> score = (1 + 4) * 2
+     -------------------------------------------------------------------------------------*/
     private static int getTwoPairScore(int score, Map<Integer, Integer> diceCount) {
         for (Integer key : diceCount.keySet()) {
             if (diceCount.get(key) >= 2) {
